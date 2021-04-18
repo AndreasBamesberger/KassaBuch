0e17b35d21a433b100c5cf3cf783ac617d7d550e
AndreasBamesberger <43984392+AndreasBamesberger@users.noreply.github.com>
2021-04-18 16:02:57 +0200

    fixed paths, added todo

--- main.py
+++ main.py
@@ -1,6 +1,18 @@
 import backend
 from gui import Application
 
+# TODO: combo box für Zahlungsart
+# TODO: rk, Mengenrabatt, Aktion für save template nicht speichern
+# TODO: komma statt Punkt
+# TODO: komma statt Punkt
+# TODO: Auch in total
+# TODO: Bindestrich für Uhrzeit
+# TODO: ausgerechnete Felder auch in gui
+# TODO: Rundungsfehler einbauen
+# TODO: Strichpunkt als delimiter
+# TODO: differenz zwischen 2 total Feldern
+# TODO: wenn combobox input leer dann alles löschen
+# TODO: encoding abdrehen
 
 if __name__ == '__main__':
     backend.CONFIG_DICT = backend.read_config("config.json")
