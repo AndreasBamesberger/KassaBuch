87fea0ecf837a86e2fb3019223b0f4e8a0e319d4
AndreasBamesberger <43984392+AndreasBamesberger@users.noreply.github.com>
2021-05-06 16:52:19 +0200

    basic price plot

--- plot_price.py
+++ plot_price.py
@@ -2,8 +2,10 @@
 Open product file, extract final_price_per_unit and date_time of all entries in
 the history and plot it
 """
-import matplotlib
+from matplotlib import pyplot
+from matplotlib import dates
 import json
+import datetime
 
 import backend
 
@@ -14,12 +16,18 @@ with open(path, 'r', encoding="utf-16") as in_file:
     data = json.load(in_file)
 
 prices = []
-dates = []
+date_list = []
 for item in data["history"]:
-    print(item)
-    print("price: ", item["price_final_per_unit"])
     prices.append(item["price_final_per_unit"])
-    dates.append(item["date_time"])
+    date_list.append(item["date_time"])
 
-print("prices: ", prices)
-print("dates: ", dates)
+# Strip time from date_list
+date_list = [item.split('T')[0] for item in date_list]
+
+# x = [datetime.datetime.strptime(d, '%Y-%m-%d').date() for d in date_list]
+# pyplot.gca().xaxis.set_major_formatter(dates.DateFormatter('%Y-%m-%d'))
+# pyplot.gca().xaxis.set_major_locator(dates.DayLocator())
+
+pyplot.plot(date_list, prices)
+# pyplot.gcf().autofmt_xdate()
+pyplot.show()
